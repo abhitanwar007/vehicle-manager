3c5c5c2c45ec9ff74133aebb74618f452ca54c57
Abhishek kumar <abhi.k.tanwar@gmail.com>
2023-04-23 13:18:43 +0530

    Add stop method

--- src/PetrolEngine.java
+++ src/PetrolEngine.java
@@ -4,4 +4,7 @@ public class PetrolEngine {
 	public void start(){
 		System.out.println("Petrol Engine Started");
 	}
+	public void stop(){
+		System.out.println("Petrol Engine stoped");
+	}
 }
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -4,7 +4,10 @@ public class Vehicle {
 	public static void main(String[] args) {
 		PetrolEngine pe = new PetrolEngine();
 		pe.start();
+		pe.stop();
+		
 		DieselEngine de = new DieselEngine();
 		de.start();
+		de.stop();
 	}
 }
