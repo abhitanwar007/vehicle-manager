5363faf88473244edf8a27de157ea57cbf281d6b
Abhishek kumar <abhi.k.tanwar@gmail.com>
2023-04-23 14:54:29 +0530

    stop method added to hydrogen engine

--- src/HydrogenEngine.java
+++ src/HydrogenEngine.java
@@ -3,4 +3,7 @@ public class HydrogenEngine {
 	public void start() {
 		System.out.println("Hydrogen engine Started");
 	}
+	public void stop() {
+		System.out.println("Hydrogen engine stoped");
+	}
 }
